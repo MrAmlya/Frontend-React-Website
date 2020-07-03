eb3f61886489fda22d878b98a9ab789a03b0cff8
Amol Bhadane <asbhadane21@gmail.com>
2020-07-03 23:21:08 +0530

    First Commit

---
+++ build/precache-manifest.20cae5d376f795272549a22185b6ad76.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a6a912d9b46688ea06b26af1680da02",
+    "url": "/index.html"
+  },
+  {
+    "revision": "493da2cc0fbd4e909047",
+    "url": "/static/css/2.4173b2d3.chunk.css"
+  },
+  {
+    "revision": "fc7d540b0d365bd49562",
+    "url": "/static/css/main.17322a8a.chunk.css"
+  },
+  {
+    "revision": "493da2cc0fbd4e909047",
+    "url": "/static/js/2.af0b6672.chunk.js"
+  },
+  {
+    "revision": "3a5359d431b8190dc2f36e4703f39768",
+    "url": "/static/js/2.af0b6672.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fc7d540b0d365bd49562",
+    "url": "/static/js/main.a9f31a5f.chunk.js"
+  },
+  {
+    "revision": "84926f692196015996db",
+    "url": "/static/js/runtime-main.c71f3b90.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
